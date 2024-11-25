e748153c30ab5ebf9a2e208f872092e1daf3319f
Pierre Da Silva Coelho <p.coelho@lapinou.tech>
2024-11-25 09:38:42 +0100

    Update post-deployment script path in deploy.sh for improved portability

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ curl -X POST "https://webhooks.hostinger.com/deploy/b5e3bd8244942df0fe0b3f28a1ec
      -H "Content-Type: application/json" \
      -d "{\"branch\": \"$1\", \"action\": \"deploy\"}"
 
-POST_DEPLOYMENT_SCRIPT="/home/u309746653/domains/lapinou.tech/public_html/api/scripts/post-deployment.sh"
+POST_DEPLOYMENT_SCRIPT="./scripts/post-deployment.sh"
 
 if [ -f "$POST_DEPLOYMENT_SCRIPT" ]; then
   echo "Running post-deployment script..."
